40adecc9763a36d3d1c6ab0c269971858ba8c203
André Cruz <andremiguelcruz@msn.com>
2013-07-16 11:43:27 +0100

    components -> bower_components.

--- app/loader.js
+++ app/loader.js
@@ -3,15 +3,15 @@
 requirejs.config({
     baseUrl: './dev/src',
     paths: {
-        'mout': '../components/mout/src',
-        'events-emitter': '../components/events-emitter/src',
-        'address': '../components/address/src',
-        'jquery': '../components/jquery/jquery',
-        'doT': '../components/doT/doT',
-        'text': '../components/requirejs-text/text',
-        'has': '../components/has/has',
-        'bootstrap': '../components/bootstrap/js/bootstrap',
-        'bootstrap-css': '../components/bootstrap/css'
+        'mout': '../bower_components/mout/src',
+        'events-emitter': '../bower_components/events-emitter/src',
+        'address': '../bower_components/address/src',
+        'jquery': '../bower_components/jquery/jquery',
+        'doT': '../bower_components/doT/doT',
+        'text': '../bower_components/requirejs-text/text',
+        'has': '../bower_components/has/has',
+        'bootstrap': '../bower_components/bootstrap/js/bootstrap',
+        'bootstrap-css': '../bower_components/bootstrap/css'
     },
     shim: {
         'handlebars': {
@@ -24,23 +24,23 @@ requirejs.config({
             'app-config': '../app/config/config_dev',
 
             // Spoon
-            'spoon': '../components/spoonjs/src/index',
+            'spoon': '../bower_components/spoonjs/src/index',
 
             // Spoon aliases
-            'spoon/Controller': '../components/spoonjs/src/core/Controller',
-            'spoon/View': '../components/spoonjs/src/core/View',
+            'spoon/Controller': '../bower_components/spoonjs/src/core/Controller',
+            'spoon/View': '../bower_components/spoonjs/src/core/View',
 
             // Spoon services
-            'services/broadcaster': '../components/spoonjs/src/core/Broadcaster/BroadcasterFactory',
-            'services/address': '../components/spoonjs/src/core/Address/AddressFactory',
-            'services/state': '../components/spoonjs/src/core/StateRegistry/StateRegistryFactory'
+            'services/broadcaster': '../bower_components/spoonjs/src/core/Broadcaster/BroadcasterFactory',
+            'services/address': '../bower_components/spoonjs/src/core/Address/AddressFactory',
+            'services/state': '../bower_components/spoonjs/src/core/StateRegistry/StateRegistryFactory'
         }
     },
     packages: [
         // CSS plugin
         {
             name: 'css',
-            location: '../components/require-css',
+            location: '../bower_components/require-css',
             main: 'css'
         }
     ]
